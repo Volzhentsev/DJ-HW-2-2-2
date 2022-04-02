d540c6f99f738ddf091464986973eb4a0ba22e5f
Volzhentsev Sergey <volzhentsev@bk.ru>
2022-04-02 23:58:21 +0300

    fist commit

---
+++ articles/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from articles.models import Article
+
+
+def articles_list(request):
+    articles = Article.objects.all()
+    template = 'articles/news.html'
+    context = {'object_list': articles}
+
+    return render(request, template, context)
